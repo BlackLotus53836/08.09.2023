2e5f413f9e5cce16647e7bbe74d908f527935d9b
Mark <Loop>
2023-09-08 19:11:02 +0300

    Добавьте файлы проекта.

---
+++ 08.09.2023/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество чисел: ");
+        int count = int.Parse(Console.ReadLine());
+
+        int positiveCount = 0;
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write($"Введите число {i + 1}: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+            {
+                positiveCount++;
+            }
+        }
+
+        Console.WriteLine($"Количество положительных чисел: {positiveCount}");
+    }
+}
